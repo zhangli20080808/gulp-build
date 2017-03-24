73a5411d633b4a9ecddaf48e53c4508bf1ad305e
力力娃 <liliwa@liliwadeMacBook-Pro.local>
2017-03-24 16:13:31 +0800

    gulp自动化工具构建测试

---
+++ gulpfile.js
@@ -0,0 +1,105 @@
+'use strict';
+/**
+ * 1. LESS编译 压缩 合并
+ * 2. JS合并 压缩 混淆
+ * 3. img复制
+ * 4. html压缩
+ */
+
+// 在gulpfile中先载入gulp包，因为这个包提供了一些API
+var gulp = require('gulp');
+var less = require('gulp-less');
+var cssnano = require('gulp-cssnano');
+
+
+gulp.task('lib', function () {
+    gulp.src('bower_components/**/*')
+        .pipe(gulp.dest('dist/' + 'vendor'))
+        .pipe(gulp.dest('build/' + 'vendor'))
+});
+
+
+// 1. LESS编译 压缩 --合并没有必要，一般预处理CSS都可以导包
+gulp.task('style', function () {
+    // 这里是在执行style任务时自动执行的
+    gulp.src(['src/styles/*.less', '!src/styles/_*.less'])
+        .pipe(less())
+        .pipe(gulp.dest('build/styles'))
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/styles'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+// 2. JS合并 压缩混淆
+gulp.task('script', function () {
+    gulp.src('src/scripts/**/*.js')
+        .pipe(concat('index.js'))
+        .pipe(gulp.dest('build/scripts'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/scripts'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+// 3. 图片复制
+var imagemin = require('gulp-imagemin');
+gulp.task('image', function () {
+    gulp.src('src/images/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+        .pipe(gulp.dest('build/images'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+var htmlmin = require('gulp-htmlmin');
+// 4. HTML
+gulp.task('html', function () {
+    gulp.src('src/*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('build'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+//gulp-clean
+var clean = require('gulp-clean');
+gulp.task('clean', function () {
+    gulp.src(['dist', 'build'])
+        .pipe(clean())
+})
+//lib
+
+
+gulp.task('build', ['image', 'script', 'style', 'lib', 'html']);
+
+var browserSync = require('browser-sync');
+gulp.task('serve', ['build'], function () {
+    browserSync({
+        server: {
+            baseDir: ['dist']
+        },
+    }, function (err, bs) {
+        console.log(bs.options.getIn(["urls", "local"]));
+    });
+
+    gulp.watch('src/styles/*.less', ['style']);
+    gulp.watch('bower_components/**/*', ['lib']);
+    gulp.watch('src/scripts/**/*.js', ['script']);
+    gulp.watch('src/images/*.*', ['image']);
+    gulp.watch('src/*.html', ['html']);
+});
+
+gulp.task('default', ['serve']);
